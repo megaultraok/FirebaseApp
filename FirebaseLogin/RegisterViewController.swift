9aa0809452dbffe0042dedd90c9c2c6428ca69a7
Karan Gandhi <kag354@rutgers.edu>
2021-03-25 10:09:36 -0400

    With RegisterViewController

--- FirebaseLogin/RegisterViewController.swift
+++ FirebaseLogin/RegisterViewController.swift
@@ -6,16 +6,49 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        //Optional Binding
+        
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+          
+                if let e = error {
+                    print(e)
+                    
+                    
+                }
+                
+                else {
+                    
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    
+                }
+                
+            }
+            
+        }
+     
+        
+        
+        
+      
+    }
+    
     /*
     // MARK: - Navigation
 

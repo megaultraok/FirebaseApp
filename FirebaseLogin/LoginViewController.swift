a5870cf4f1f4e2712c2ae729cfc8fac83424e5c2
Karan Gandhi <kag354@rutgers.edu>
2021-03-25 10:33:16 -0400

    with Login View Controller

--- FirebaseLogin/LoginViewController.swift
+++ FirebaseLogin/LoginViewController.swift
@@ -1,55 +1,55 @@
 //
-//  RegisterViewController.swift
+//  LoginViewController.swift
 //  FirebaseLogin
 //
-//  Created by Karan Gandhi on 3/23/21.
+//  Created by Karan Gandhi on 3/25/21.
 //
 
 import UIKit
 import Firebase
 
-class RegisterViewController: UIViewController {
+class LoginViewController: UIViewController {
+
     @IBOutlet weak var emailTextField: UITextField!
+    
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
+    
+    
     @IBAction func buttonPressed(_ sender: Any) {
-        
-        //Optional Binding
-        
         
         if let email = emailTextField.text, let password = passwordTextField.text {
             
-            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-          
-                if let e = error {
+            
+            Auth.auth().signIn(withEmail: email, password: password) {authResult, error in
+      
+              // ...
+           
+                
+                if let e = error{
                     print(e.localizedDescription)
-                    
-                    
                 }
                 
                 else {
                     
-                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
-                    
+                    self.performSegue(withIdentifier: "LoginToChat" , sender: self)
                 }
                 
-            }
-            
+                
+                
         }
-     
         
-        
-        
-      
     }
     
+
     /*
+     
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -59,4 +59,5 @@ class RegisterViewController: UIViewController {
     }
     */
 
+}
 }

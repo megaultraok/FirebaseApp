1634e1e171937211f41507055f35fcb4033b59c4
Jada White <jadaw.2021@gmail.com>
2021-04-08 13:59:43 -0400

    Added IQKeyboardManager package dependency.

--- FirebaseLogin/AppDelegate.swift
+++ FirebaseLogin/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         
         let db = Firestore.firestore()
+        
+        IQKeyboardManager.shared.enable = true
 
 
         return true
